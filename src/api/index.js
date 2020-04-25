f7b9cfebef04bacdbfd3e9dfdf40b27027f0f72f
romluc <romulofilhofilho@gmail.com>
2020-04-25 18:48:44 -0300

    Create CountryPicker component

--- src/api/index.js
+++ src/api/index.js
@@ -22,7 +22,24 @@ export const fetchData = async () => {
 export const fetchDailyData = async () => {
   try {
     const { data } = await axios.get(`${url}/daily`);
-    console.log(data);
+    const modifiedDate = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifiedDate;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name);
   } catch (error) {
     console.log(error);
   }
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+import styles from './CountryPicker.module.css';
+import { fetchCountries } from '../../api';
 
 const CountryPicker = () => {
-  return <h1>CountryPicker</h1>;
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchedAPI = async () => {
+      setFetchedCountries(await fetchCountries());
+    };
+
+    fetchedAPI();
+  }, [setFetchedCountries]);
+
+  return (
+    <FormControl>
+      <NativeSelect>
+        <option value='global'>Global</option>
+        {fetchedCountries.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
 };
 
 export default CountryPicker;
